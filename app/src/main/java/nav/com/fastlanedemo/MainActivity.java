721810582378f6d9af706254cfa7dd0ac5b896bc
Navdeep <navdeepg@kater.com>
2019-04-09 22:59:00 -0700

    Initial commit

---
+++ app/src/main/java/nav/com/fastlanedemo/MainActivity.java
@@ -0,0 +1,18 @@
+package nav.com.fastlanedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Fabric.with(this, new Crashlytics());
+	}
+}
