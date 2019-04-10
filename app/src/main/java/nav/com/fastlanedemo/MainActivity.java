da18eee035f22fb40844050aec6d5a411e26d859
Navdeep <navdeepg@kater.com>
2019-04-10 14:35:07 -0700

    Added notes to lane Trigger

--- app/src/main/java/nav/com/fastlanedemo/MainActivity.java
+++ app/src/main/java/nav/com/fastlanedemo/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		Log.e("Test","Test");
-		Log.e("Test","Test2");
+		Log.e("Test","Test23");
 		Fabric.with(this, new Crashlytics());
 	}
 }
